8364f549e19e1a364b2a4f6abb656ff9f8946729
proalamin <pro.alamin247@gmail.com>
2023-03-20 11:43:09 +0600

    storage-factory

---
+++ learn blockchain & solidity/storage-factory/SimpleStorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+// EVM, Ethereum Virtual Machine
+// Avalanche, Fantom, Polygon
+
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // uint256[] public favoriteNumbersList;
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public virtual{
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
+
